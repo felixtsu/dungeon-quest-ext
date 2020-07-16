001e67942df70e7d56501f73fa40b9fa06fff70d
felixtsu <humaeks@gmail.com>
2020-07-16 16:03:47 +0800

    lvl1

--- main.ts
+++ main.ts
@@ -1,12 +1,14 @@
 //% weight=99 color="#6d5ba5" icon="\uf004"
 //%groups='["Skill", "Game"]'
 namespace dungeon {
-    let playerSprite:Sprite = null
+    let playerSprite: Sprite = null
+    const SPRITE_KIND_WATER_BALL = SpriteKind.create()
+    const SPRITE_KIND_PLAYER = SpriteKind.create()
 
     //%block
     //%group="Skill"
     export function throwWaterBallRight() {
-        sprites.createProjectileFromSprite(img`
+        let waterballSprite = sprites.createProjectileFromSprite(img`
             . . . . . . . . . . . . . . . .
             . . . . . . . . . . . . . . . .
             . . . . . . . . . . . . . . . .
@@ -24,11 +26,12 @@ namespace dungeon {
             . . . . . . . . . . . . . . . .
             . . . . . . . . . . . . . . . .
         `, playerSprite, 50, 0)
+        waterballSprite.setKind(SPRITE_KIND_WATER_BALL)
     }
     //%block
     //%group="Skill"
     export function throwWaterBallDown() {
-        sprites.createProjectileFromSprite(img`
+        let waterballSprite = sprites.createProjectileFromSprite(img`
             . . . . . . . . . . . . . . . .
             . . . . . . . . . . . . . . . .
             . . . . . . . . . . . . . . . .
@@ -46,11 +49,12 @@ namespace dungeon {
             . . . . . . . . . . . . . . . .
             . . . . . . . . . . . . . . . .
         `, playerSprite, 0, -50)
+        waterballSprite.setKind(SPRITE_KIND_WATER_BALL)
     }
     //%block
     //%group="Skill"
     export function throwWaterBallUp() {
-        sprites.createProjectileFromSprite(img`
+        let waterballSprite = sprites.createProjectileFromSprite(img`
             . . . . . . . . . . . . . . . .
             . . . . . . . . . . . . . . . .
             . . . . . . . . . . . . . . . .
@@ -68,11 +72,12 @@ namespace dungeon {
             . . . . . . . . . . . . . . . .
             . . . . . . . . . . . . . . . .
         `, playerSprite, 0, -50)
+        waterballSprite.setKind(SPRITE_KIND_WATER_BALL)
     }
     //%block
     //%group="Skill"
     export function throwWaterBallLeft() {
-        sprites.createProjectileFromSprite(img`
+        let waterballSprite = sprites.createProjectileFromSprite(img`
             . . . . . . . . . . . . . . . .
             . . . . . . . . . . . . . . . .
             . . . . . . . . . . . . . . . .
@@ -90,6 +95,7 @@ namespace dungeon {
             . . . . . . . . . . . . . . . .
             . . . . . . . . . . . . . . . .
         `, playerSprite, -50, 0)
+        waterballSprite.setKind(SPRITE_KIND_WATER_BALL)
     }
 
     //%block
@@ -116,9 +122,18 @@ namespace dungeon {
                 . . . . . . . . . . . 2 . . . 2
                 . . . . . . . . . . . 2 2 2 2 2
             `,
-            [myTiles.tile0, sprites.dungeon.greenOuterNorthWest, sprites.dungeon.greenOuterNorthEast, sprites.dungeon.greenOuterSouthEast, sprites.dungeon.greenOuterSouthWest, sprites.dungeon.greenOuterEast0, sprites.dungeon.greenOuterNorth0, sprites.dungeon.greenOuterWest1, sprites.dungeon.greenOuterSouth0, sprites.dungeon.greenOuterNorth2, sprites.dungeon.greenInnerNorthWest, sprites.dungeon.greenInnerNorthEast, sprites.dungeon.greenInnerSouthWest, sprites.dungeon.greenInnerSouthEast, sprites.dungeon.greenOuterEast2, sprites.dungeon.greenOuterSouth2, sprites.dungeon.greenOuterWest2, sprites.dungeon.floorLight2, sprites.dungeon.greenOuterWest0, sprites.dungeon.greenOuterSouth1, sprites.dungeon.greenOuterEast1, sprites.dungeon.greenOuterNorth1, sprites.dungeon.floorDark2, myTiles.tile1, sprites.dungeon.chestClosed],
+            [myTiles.tile0, sprites.dungeon.greenOuterNorthWest, sprites.dungeon.greenOuterNorthEast, sprites.dungeon.greenOuterSouthEast, sprites.dungeon.greenOuterSouthWest, sprites.dungeon.greenOuterEast0, sprites.dungeon.greenOuterNorth0, sprites.dungeon.greenOuterWest1, sprites.dungeon.greenOuterSouth0, sprites.dungeon.greenOuterNorth2, sprites.dungeon.greenInnerNorthWest, sprites.dungeon.greenInnerNorthEast, sprites.dungeon.greenInnerSouthWest, sprites.dungeon.greenInnerSouthEast, sprites.dungeon.greenOuterEast2, sprites.dungeon.greenOuterSouth2, sprites.dungeon.greenOuterWest2, sprites.dungeon.floorLight2, sprites.dungeon.greenOuterWest0, sprites.dungeon.greenOuterSouth1, sprites.dungeon.greenOuterEast1, sprites.dungeon.greenOuterNorth1, sprites.dungeon.floorDark2, myTiles.tile1, sprites.dungeon.chestClosed, myTiles.tile2],
             TileScale.Sixteen
         ))
+
+
+        scene.onOverlapTile(SPRITE_KIND_PLAYER, myTiles.tile1, function (sprite: Sprite, location: tiles.Location) {
+            sprite.say("The door is locked", 1000)
+            sprite.y = sprite.y + 2
+        })
+
+
+
         playerSprite = sprites.create(img`
             . . . . . . f f f f . . . . . .
             . . . . f f f 2 2 f f f . . . .
@@ -136,10 +151,31 @@ namespace dungeon {
             . . 4 4 f 4 4 5 5 4 4 f 4 4 . .
             . . . . . f f f f f f . . . . .
             . . . . . f f . . f f . . . . .
-        `, SpriteKind.Player)
+        `, SPRITE_KIND_PLAYER)
         controller.moveSprite(playerSprite)
         tiles.placeOnTile(playerSprite, tiles.getTileLocation(14, 14))
         scene.cameraFollowSprite(playerSprite)
+    }
+
+    //%block
+    //%group="Skill"
+    export function openDoor() {
+        tiles.setTileAt(tiles.getTileLocation(13, 12), myTiles.tile2)
+    }
+
+
+    function prepareLevel2() {
+        scene.onHitWall(SPRITE_KIND_WATER_BALL, function (sprite: Sprite) {
+            if (sprite.tileKindAt(TileDirection.Right, sprites.dungeon.greenOuterEast2)) {
+                tiles.setTileAt(tiles.getTileLocation(10, 8), sprites.dungeon.greenOuterEast0)
+            } else if (sprite.tileKindAt(TileDirection.Left, sprites.dungeon.greenOuterWest2)) {
+                tiles.setTileAt(tiles.getTileLocation(4, 8), sprites.dungeon.greenOuterWest1)
+            } else if (sprite.tileKindAt(TileDirection.Top, sprites.dungeon.greenOuterNorth2)) {
+                tiles.setTileAt(tiles.getTileLocation(7, 5), sprites.dungeon.greenOuterNorth0)
+            } else if (sprite.tileKindAt(TileDirection.Bottom, sprites.dungeon.greenOuterSouth2)) {
+                tiles.setTileAt(tiles.getTileLocation(7, 11), sprites.dungeon.greenOuterSouth0)
+            }
+        })
     }
 
     namespace myTiles {
@@ -167,7 +203,7 @@ namespace dungeon {
             c c c c c c c c c c c c c c c c
             c 7 7 7 7 7 7 c c 7 7 7 7 7 7 c
             c 6 6 6 6 6 6 c c 6 6 6 6 6 6 c
-            c 6 c c c c 6 c c 6 c c c c 6 a
+            c 6 c c c c 6 c c 6 c c c c 6 c
             c 6 c 7 7 7 6 c c 6 7 7 7 c 6 c
             c 6 c 6 6 6 6 c c 6 6 6 6 c 6 c
             c 6 c 6 c c c c c c c c 6 c 6 c
@@ -178,6 +214,25 @@ namespace dungeon {
             c 6 c 6 6 6 6 c c 6 6 6 6 c 6 c
             c 6 c c c c 6 c c 6 c c c c 6 c
             c 6 6 6 6 6 6 c c 6 6 6 6 6 6 c
+            c c c c c c c c c c c c c c c c
+            c c c c c c c c c c c c c c c c
+        `
+        //% blockIdentity=images._tile
+        export const tile2 = img`
+            c c 7 6 c c 7 c c 7 c c 6 7 c c
+            c 7 6 c c 7 6 c c 6 7 c c c 6 c
+            c 6 c c 7 6 6 c c 6 6 7 c c 6 c
+            c 6 c 7 6 c 6 c c 6 c 6 7 c 6 c
+            c 6 c 6 c c 6 c c 6 c c 6 c 6 c
+            c 6 c 6 c 6 6 c c 6 c c 6 c 6 c
+            c 6 c 6 c c 6 c c 6 c c 6 c 6 c
+            c 6 c 6 c 6 6 c c 6 6 c 6 c 6 c
+            c 6 c 6 6 6 6 c c 6 6 6 6 c 6 c
+            c 6 c 6 6 c 6 c c 6 c 6 6 c 6 c
+            c 6 c 6 c c 6 c c 6 c c 6 c 6 c
+            c 6 c c c 6 6 c c 6 6 c c c 6 c
+            c 6 c c 6 6 c c c c 6 6 c c 6 c
+            c 6 6 6 6 c c c c c c 6 6 6 6 c
             c c c c c c c c c c c c c c c c
             c c c c c c c c c c c c c c c c
         `
