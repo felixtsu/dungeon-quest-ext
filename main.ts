f4911adff3e40e007303561691be2f7344661577
felixtsu <humaeks@gmail.com>
2020-07-21 15:16:57 +0800

    Eng version.

--- main.ts
+++ main.ts
@@ -21,16 +21,19 @@ namespace dungeon {
     //%group="Skill"
     export function cook(recipe: Recipe) {
         if (!playerSprite.tileKindAt(TileDirection.Center, myTiles.tile2)) {
-            playerSprite.say('我要在火堆上才能做饭', 1000)
+            // playerSprite.say('我要在火堆上才能做饭', 1000)
+            playerSprite.say('I can only cook at campfire.', 1000)
             return
         }
 
         if (ingredientsAcquired != 2 || recipe != correctRecipe) {
-            playerSprite.say("你身上的原料做不出来这个菜，你饿死了", 1000)
+            // playerSprite.say("你身上的原料做不出来这个菜，你饿死了", 1000)
+            playerSprite.say("I don't have ingredient for that, I'm starving.", 1000)
             pause(1000)
             game.over()
         } else {
-            playerSprite.say("饱餐一顿的勇者第二天就找到了出路", 2000)
+            // playerSprite.say("饱餐一顿的勇者第二天就找到了出路", 2000)
+            playerSprite.say("Nice meal, I can see a way out.", 2000)
             pause(2000)
             game.over(true)
         }
@@ -49,7 +52,8 @@ namespace dungeon {
         if (controller.up.isPressed() || controller.down.isPressed() || 
             controller.right.isPressed() || controller.left.isPressed() || 
             moving) {
-            playerSprite.say("我要站着不动才能发射水球", 500)
+            // playerSprite.say("我要站着不动才能发射水球", 500)
+            playerSprite.say("I can't throw a water ball while moving", 500)
             return
         }
         let powerBarSprite = statusbars.getStatusBarAttachedTo(StatusBarKind.Energy, playerSprite)
@@ -247,7 +251,8 @@ namespace dungeon {
         tiles.placeOnTile(trapSprite, tiles.getTileLocation(13, 11))
 
         sprites.onOverlap(SpriteKind.Player, SPRITE_KIND_TRAP, function (sprite: Sprite, otherSprite: Sprite) {
-            game.splash("你作弊破门被发现，即死")
+            game.splash("You cheat breaking the door, the dungeon master cast instant death on you.")
+            // game.splash("你作弊破门被发现，即死")
             game.over()
         })
     }
@@ -363,12 +368,14 @@ namespace dungeon {
     //%group="Skill"
     export function openDoor() {
         if (!playerSprite.overlapsWith(doorSprite)) {
-            playerSprite.say("我要走到门旁边才能开门")
+            playerSprite.say("There's no door around.")
+            // playerSprite.say("我要走到门旁边才能开门")
             return;
         }
 
         if (sprites.allOfKind(SpriteKind.Key).length != 0) {
-            game.splash("想要暴力推门，结果被门砸死了")
+            game.splash("Pushing the door by brutal force, it collapsed and killed you. ")
+            // game.splash("想要暴力推门，结果被门砸死了")
             game.over()
         }
 
@@ -538,7 +545,8 @@ namespace dungeon {
         prepareRandomIngredients()
 
         tiles.placeOnTile(playerSprite, tiles.getTileLocation(3, 2))
-        playerSprite.say('这里有篝火，可以做饭, 找找看附近有没有材料', 2000)
+        playerSprite.say("There's campfire for cooking, let me find some ingredients.", 2000)
+        // playerSprite.say('这里有火堆，可以做饭, 找找看附近有没有材料', 2000)
 
     }
 
